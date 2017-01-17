c37fa04dfa736db03cc8e1b8983ccc4f49de8f70
wurui <wurui.wr@gmail.com>
2017-01-17 16:57:55 +0800

    sss

--- asset/index.js
+++ asset/index.js
@@ -1,7 +1,119 @@
-define([],function(){
-  return {
-    init:function($mod){
+define(['oxjs'], function (OX) {
+    var targetDS = '/mystock-analysis@2587dcf6c630406d73ce62959';
+    //targetDS='/mystock-analysis@2587dd26433e5da9b76d1aced'
+    OX.config({devHost:'//local.openxsl.com'})
+    var uid;
+    var syncRemote = function (data) {
+            //2587dc2a8630406d73ce62957
+            var callbackcounter = data.length,
+                afterSave = function () {
+                    callbackcounter--;
+                    if (!callbackcounter) {
+                        console.log('done')
+                    }
+                };
+            for (var i = 0; i < data.length; i++) {
+                var n = data[i];
+                OX.callapi('update' + targetDS, {
+                    query: {
+                        symbol: n.symbol
+                    },
+                    update: {
+                        $set: {
+                            med: n.med,
+                            close: n.close,
+                            avg: n.avg,
+                            lastDate: n.lastDate
+                        },
+                        $addToSet:{
+                            "user":uid
+                        }
+                    }
+
+                }, afterSave);
+            }
+
+        },
+        filltr = function (tr, data) {
+            var percent = (data.medLow > 0 ? '+' : '') + (data.medLow * 100).toFixed(1) + '%'
+            data.percent = '<em class="percent ' + (data.medLow > 0 ? 'positive' : 'negative') + '">' + percent + '</em>'
+            $(tr).find('[data-key]').each(function () {
+                var key = this.getAttribute('data-key');
+                this.innerHTML = data[key] || ''
+            })
+        },
+        syncDate = function (trs) {
+            var symbols = [], domCache = {};
+            for (var i = 0, tr; tr = trs[i++];) {
+                var symbol = tr.getAttribute('data-href');
+                symbols.push(symbol);
+                domCache[symbol] = tr;
+            }
+            OX.getJSON('http://momofox.com:8000/analyze/querymaybelow?symbols=' + symbols.join(','), function (r) {
+
+                //console.log(r);
+
+                for (var i = 0, n; n = r[i++];) {
+                    var tr = domCache[n.symbol];
+                    filltr(tr, n)
+                }
+                syncRemote(r);
+
+
+            });
+
+
+        },
+        checkDate = function ($mod) {
+            OX.getJSON('http://momofox.com:8000/historical/getlastdates?limit=1', function (r) {
+                var target_date = r.data[0],
+                    trs = [];
+                //console.log(target_date)
+
+                $mod.find('tr[data-date]').each(function (i, n) {
+                    var date = n.getAttribute('data-date');
+                    if (date != target_date) {
+                        trs.push(n);
+                    }
+                });
+                syncDate(trs)
+
+            });
+        };
+    return {
+        init: function ($mod) {
+            uid=$mod.attr('data-uid')
+
+            var lastFocusNode, cls = 'selected';
+            var qs = $mod.attr('data-qs'),
+                param = {},
+                checkSelect = function () {
+                    if (lastFocusNode)lastFocusNode.removeClass(cls);
+                    var symbol = OX.queryString(qs);//console.log(symbol)
+                    if (symbol) {
+                        lastFocusNode = $mod.find('tr[data-href="' + symbol + '"]').addClass(cls);
+                    }
+                };
+            $mod.on('tap', '[data-href]', function (e) {
+                e.preventDefault();
+                //console.log('clicked');
+                /*
+                 if (lastFocusNode)lastFocusNode.removeClass(cls);
+                 lastFocusNode = $(this).addClass(cls);
+                 */
+
+                param[qs] = this.getAttribute('data-href');
+
+                OX.changeState(param)
+
+                //location.href = this.getAttribute('data-href')
+            });
+            OX.onstatechanged(checkSelect);
+
+            checkSelect();
+
+            checkDate($mod)
+        }
 
     }
-  }
 })
--- mod.xsl
+++ mod.xsl
@@ -1,13 +1,43 @@
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:oxm="https://www.openxsl.com">
-
+    <xsl:template match="*" name="percent">
+        <xsl:param name="delta"/>
+        <xsl:choose>
+            <xsl:when test="$delta &gt; 0">
+                <em class="percent positive">+<xsl:value-of select="format-number($delta,'0.0%')"/></em>
+            </xsl:when>
+            <xsl:otherwise>
+                <em class="percent negative"><xsl:value-of select="format-number($delta,'0.0%')"/></em>
+            </xsl:otherwise>
+        </xsl:choose>
+    </xsl:template>
     <xsl:template match="/root" name="wurui.stocksync">
-    <!-- className 'J_OXMod' required  -->
-    <div class="J_OXMod oxmod-stocksync" ox-mod="stocksync">
+        <xsl:param name="qs">symbol</xsl:param>
+        <!-- className 'J_OXMod' required  -->
+        <div class="J_OXMod oxmod-stocksync" data-uid="{login/uid}" ox-mod="stocksync" data-qs="{$qs}">
 
-      <h1>
-        This is mod stocksync;
-      </h1>
-      </div>
+            <table cellpadding="0" cellspacing="0" class="maintable">
+                <tbody>
+                    <xsl:for-each select="data/mystock-analysis/i">
+                        <tr data-href="{normalize-space(symbol)}" data-date="{lastDate}">
+                            <td data-key="symbol">
+                                <xsl:value-of select="symbol"/>
+                            </td>
+                            <td data-key="med">
+                                <xsl:value-of select="med"/>
+                            </td>
+                            <td align="right">
+                                <span data-key="close"><xsl:value-of select="close"/></span>
+                                <span data-key="percent">
+                                <xsl:call-template name="percent">
+                                    <xsl:with-param name="delta" select="(close - med) div med"/>
+                                </xsl:call-template>
+                                </span>
+                            </td>
+                        </tr>
+                    </xsl:for-each>
+                </tbody>
+            </table>
+        </div>
     </xsl:template>
 
 </xsl:stylesheet>
